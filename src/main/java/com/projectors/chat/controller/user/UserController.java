8a10c9a744e6836431e612aacf7be9dc8072cf2d
JuYeon-Jin <juuju.00b@gmail.com>
2024-07-11 01:55:44 +0900

    240711 update

--- src/main/java/com/projectors/chat/controller/user/UserController.java
+++ src/main/java/com/projectors/chat/controller/user/UserController.java
@@ -3,10 +3,12 @@ package com.projectors.chat.controller.user;
 
 import com.projectors.chat.dto.request.user.UserCreateReq;
 import com.projectors.chat.service.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class UserController {
@@ -24,7 +26,10 @@ public class UserController {
 
     @PostMapping("/login")
     public String login(Model model, UserCreateReq dto) {
-        userService.login(dto);
+        boolean isSuccessful = userService.login(dto);
+        if (!isSuccessful) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "입력하신 정보가 일치하지 않습니다. 확인 후 다시 로그인해 주세요.");
+        }
         return "/view/project/home";
     }
 
@@ -35,6 +40,10 @@ public class UserController {
 
     @PostMapping("/join")
     public String join(UserCreateReq dto) {
+        boolean isSuccessful = userService.join(dto);
+        if (!isSuccessful) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "회원가입이 정상적으로 처리되지 않았습니다. 다시 시도해 주세요.");
+        }
         return "redirect:/";
     }
 
