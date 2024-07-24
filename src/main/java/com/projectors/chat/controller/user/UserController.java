1b1df9eecfe15256fe95bd53f10221387e11e953
JuYeon-Jin <juuju.00b@gmail.com>
2024-07-24 23:06:40 +0900

    RestController 변경

--- src/main/java/com/projectors/chat/controller/user/UserController.java
+++ src/main/java/com/projectors/chat/controller/user/UserController.java
@@ -4,47 +4,51 @@ package com.projectors.chat.controller.user;
 import com.projectors.chat.dto.request.user.UserCreateReq;
 import com.projectors.chat.service.user.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
+    // UserController 생성자, UserService 를 주입 받음
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    public String mainPage() {
-        return "/view/user/home";
-    }
-
+    /**
+     * 사용자 로그인 엔드 포인트
+     *
+     * @param dto UserCreateReq 객체, 사용자 로그인 정보
+     * @return 로그인 성공 여부에 따른 ResponseEntity
+     */
     @PostMapping("/login")
-    public String login(UserCreateReq dto) {
+    public ResponseEntity<?> login(@RequestBody UserCreateReq dto) {
         boolean isSuccessful = userService.login(dto);
         if (!isSuccessful) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "입력하신 정보가 일치하지 않습니다. 확인 후 다시 로그인해 주세요.");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("입력하신 정보가 일치하지 않습니다. 확인 후 다시 로그인해 주세요.");
+        } else {
+            return ResponseEntity.ok(true);
         }
-        return "forward:/main";
     }
 
-    @GetMapping("/join")
-    public String join() {
-        return "/view/user/join";
-    }
-
+    /**
+     * 사용자 회원 가입 엔드 포인트
+     *
+     * @param dto UserCreateReq 객체, 사용자 회원 가입 정보
+     * @return 회원 가입 성공 여부에 따른 ResponseEntity
+     */
     @PostMapping("/join")
-    public String join(UserCreateReq dto) {
+    public ResponseEntity<?> join(@RequestBody UserCreateReq dto) {
         boolean isSuccessful = userService.join(dto);
         if (!isSuccessful) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "회원가입이 정상적으로 처리되지 않았습니다. 다시 시도해 주세요.");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("회원가입이 정상적으로 처리되지 않았습니다. 다시 시도해 주세요.");
+        } else {
+            return ResponseEntity.ok(true);
         }
-        return "redirect:/";
     }
 
 }
